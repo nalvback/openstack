b32aa3c0978eb4d73d6d77ef1b4258107956b4c6
nalvback <alvback@gmail.com>
2016-11-16 18:04:17 +0100

    initial commit

---
+++ keystone_credentials.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+
+def get_keystone_credentials():
+    d = {}
+    d['username'] = os.environ['OS_USERNAME']
+    d['password'] = os.environ['OS_PASSWORD']
+    d['auth_url'] = os.environ['OS_AUTH_URL']
+    return d
+
+
+
+
